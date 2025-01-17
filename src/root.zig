20cdc0bfeffaa9dce6385718e881c11533798ad5
TheDrJosh <51982649+TheDrJosh@users.noreply.github.com>
2025-01-17 00:08:19 -0800

    change c include location temperarly

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
-pub const c = @import("c.zig");
+// pub const c = @import("c.zig");
+
+pub const c = @cImport(@cInclude("blend2d.h"));
 
 const e = @import("error.zig");
 
 pub const Blend2dError = e.Blend2DError;
 
-pub const Context = @import("context.zig");
-pub const Image = @import("image.zig");
+// pub const Context = @import("context.zig");
+// pub const Image = @import("image.zig");
